26544390476e20633800f32d37a0361d993ab071
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-20 17:19:49 +0600

    Create Differentiation.py

---
+++ Differentiation.py
@@ -0,0 +1,8 @@
+choice = input("Enter your choice (1-4): ")
+    if choice == '1':
+        # Calculus problem
+        equation_input = input("Enter your calculus equation in terms of x (e.g., sin(x) + ln(x)): ")
+        f = sp.sympify(equation_input)
+        
+        # Calculate derivative
+        derivative_f = sp.diff(f, x)
