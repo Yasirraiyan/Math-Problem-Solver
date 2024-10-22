098ebfaf3070dbcfdb18c0a63ccffc444f799f80
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-22 21:46:24 +0600

    Create Calculate percentage of any number.py

---
+++ Calculate percentage of any number.py
@@ -0,0 +1,8 @@
+# Calculate percentage of a number
+
+ # 15%
+def calculatepercentage(number):
+result = (percentage / 100) * number
+print(result)  
+return result 
+number=float(input("Enter any number")
