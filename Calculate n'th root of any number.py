b00fe8a0127a3bb2b883d34ff5761ada4d95c9b8
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-22 21:53:15 +0600

    Create Calculate n'th root of any number.py

---
+++ Calculate n'th root of any number.py
@@ -0,0 +1,6 @@
+def nth_root(number, n):
+    return number ** (1 / n)
+number=int(input("Enter any number")
+# Example usag
+result = nth_root(number, n)
+print(f"The {n}-th root of {number} is {result}")
