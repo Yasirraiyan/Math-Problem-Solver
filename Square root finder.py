d83607db578a1bcb5f376f8f636590046689d8e1
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-22 14:29:18 +0600

    Create Square root finder.py

---
+++ Square root finder.py
@@ -0,0 +1,10 @@
+import math
+
+# Take input from user
+num = float(input("Enter a number: "))
+
+# Calculate the square root
+sqrt_num = math.sqrt(num)
+
+# Print the result
+print(f"The square root of {num} is {sqrt_num}")
